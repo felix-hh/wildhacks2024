4d62b885f9725d3ac78b5aa5fdf670b81a2f999b
tkslucas <takayaslucas@gmail.com>
2024-04-06 18:06:39 -0500

    add gradient to navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,7 +2,7 @@
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 py-4">
+    <nav className="bg-gradient-to-r from-cyan-500 to-blue-500 py-4">
       <div className="container mx-auto flex justify-between items-center">
         <a href="/" className="text-white text-xl font-bold">Good Neighbor</a>
         <ul className="flex space-x-4">
