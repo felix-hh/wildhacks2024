fbee652a9375ec7249c80ab4329c419656ea83ce
tkslucas <takayaslucas@gmail.com>
2024-04-06 17:58:59 -0500

    added links to navbar sign up and log in

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <a href="/" className="text-white text-xl font-bold">Good Neighbor</a>
         <ul className="flex space-x-4">
-          <li><a href="#" className="text-white hover:text-gray-300">Sign Up</a></li>
-          <li><a href="#" className="text-white hover:text-gray-300">Log In</a></li>
+          <li><a href="/adduser" className="text-white hover:text-gray-300">Sign Up</a></li>
+          <li><a href="/login" className="text-white hover:text-gray-300">Log In</a></li>
         </ul>
       </div>
     </nav>
