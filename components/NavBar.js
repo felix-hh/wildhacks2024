067a2d83bbf7c43d7894319c83d4e0925e1328ca
tkslucas <takayaslucas@gmail.com>
2024-04-06 18:01:44 -0500

    added sign up as provider and sign up as customer to navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,7 +6,8 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <a href="/" className="text-white text-xl font-bold">Good Neighbor</a>
         <ul className="flex space-x-4">
-          <li><a href="/adduser" className="text-white hover:text-gray-300">Sign Up</a></li>
+          <li><a href="/addprovider" className="text-white hover:text-gray-300">Sign Up as Provider</a></li>
+          <li><a href="/adduser" className="text-white hover:text-gray-300">Sign Up as Customer</a></li>
           <li><a href="/login" className="text-white hover:text-gray-300">Log In</a></li>
         </ul>
       </div>
